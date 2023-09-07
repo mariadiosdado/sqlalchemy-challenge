e4e9bff66fc86db6c5ea501aca1b2543862f0efc
mariadiosdado <yesenia.diosdado@yahoo.com>
2023-09-07 07:10:53 -0500

    Finished climate_sqlalchemy.ipynb and started app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,90 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
+Measurement = Base.classes.measurement
 
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all precipitation data
+  
+    previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= previous_year).all()
+
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    prcp = dict(results)
+
+    return jsonify(prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all stations data
+  
+    results = session.query(Station.station).all()
+
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
