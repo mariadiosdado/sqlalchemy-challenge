042f33afb104e0fd87cbeaf7d2ade863d6b8fc94
mariadiosdado <yesenia.diosdado@yahoo.com>
2023-09-07 20:24:53 -0500

    Finished app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -53,9 +53,7 @@ def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
     # Query all precipitation data
-  
     previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
     results = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= previous_year).all()
 
@@ -73,16 +71,69 @@ def stations():
     session = Session(engine)
 
     # Query all stations data
-  
     results = session.query(Station.station).all()
 
 
     session.close()
 
-    # Convert list of tuples into dictionary
     all_stations = list(np.ravel(results))
 
     return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all stations data
+    previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+    filter(Measurement.station == "USC00519281").\
+    filter(Measurement.date >= previous_year).all()
+
+    session.close()
+
+    previous_tobs = list(np.ravel(results))
+
+    return jsonify(previous_tobs)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def stats(start=None, end=None):
+
+    # Select Statement
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    if not end:
+
+        start = dt.datetime.strptime(start, "%m%d%Y")
+
+        # compute TMIN, TAVG, TMAX for start date to end of database
+
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+
+        session.close()
+
+        # convert to list
+        temps = list(np.ravel(results))
+
+        return jsonify(temps)
+    
+    # Compute TMIN, TAVG, TMAX with start and end date
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    end = dt.datetime.strptime(end, "%m%d%Y")
+
+    results = results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+    
+    session.close()
+
+    temps = list(np.ravel(results))
+
+    return jsonify(temps=temps)
 
 
 
